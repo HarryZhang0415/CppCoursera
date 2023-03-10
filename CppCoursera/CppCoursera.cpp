0b73e398247edf46d0eeed5377eee668bd4f4a1a
harryzhang <harryzhang0415@gmail.com>
2023-03-10 13:37:24 -0500

    CppCoursera init

---
+++ CppCoursera/CppCoursera.cpp
@@ -0,0 +1,12 @@
+﻿// CppCoursera.cpp : Defines the entry point for the application.
+//
+
+#include "CppCoursera.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ CppCoursera/CppCoursera.h
@@ -0,0 +1,8 @@
+﻿// CppCoursera.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
